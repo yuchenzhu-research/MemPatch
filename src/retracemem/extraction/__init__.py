2e2bdfe9a8508833b0ecfe482e8d987b7ef78f7d
yuchenzhu-research <zyc20070222@gmail.com>
2026-05-27 23:59:27 +0800

    Implement PromptRelationVerifier, MockExtractor and CandidateRetriever protocols

---
+++ src/retracemem/extraction/__init__.py
@@ -0,0 +1,6 @@
+"""Extraction modules for ReTrace."""
+
+from retracemem.extraction.base import BeliefExtractor
+from retracemem.extraction.manual_fixture_extractor import ManualFixtureExtractor
+
+__all__ = ["BeliefExtractor", "ManualFixtureExtractor"]
