c3b9d7769cd9ab1e0f1f6f5955d285ab9fc1da20
yuchenzhu-research <zyc20070222@gmail.com>
2026-05-28 00:02:24 +0800

    Implement ReTraceBackend integration and query basis retrieval

--- src/retracemem/backends/__init__.py
+++ src/retracemem/backends/__init__.py
@@ -1,5 +1,7 @@
 """Benchmark-neutral memory backend interfaces and wrappers."""
 
 from retracemem.backends.retrieval_baseline import RetrievalBaselineBackend
+from retracemem.backends.retrace_backend import ReTraceBackend
 
-__all__ = ["RetrievalBaselineBackend"]
+__all__ = ["RetrievalBaselineBackend", "ReTraceBackend"]
+
